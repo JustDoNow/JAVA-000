482315c4c737ad4e2b600b0599aa62ef3b12955c
qizhi <emailqz@126.com>
2020-10-21 22:39:31 +0800

    custom classload, print: Hello, classLoader!

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,3 +1,6 @@
 学习笔记
 
-加油！你可以的！
+
+**参考资料**
+
+[字节码增强](https://tech.meituan.com/2019/09/05/java-bytecode-enhancement.html)
---
+++ Week_01/com/qz/MyClassLoaderTest.java
@@ -0,0 +1,22 @@
+package com.qz;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ *
+ * @author qizhi
+ * @date 2020/10/21
+ */
+public class MyClassLoaderTest {
+
+	public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+		MyClassLoader myClassLoader = new MyClassLoader("/Users/qizhi/GitHub/JAVA-000/Week_01/com/qz/", "myClassLoader");
+		final Class helloClass = myClassLoader.findClass("Hello");
+		final Object helloObject = helloClass.newInstance();
+		final Method helloMethod = helloClass.getMethod("hello", null);
+
+		// Hello, classLoader!
+		helloMethod.invoke(helloObject, null);
+	}
+}
